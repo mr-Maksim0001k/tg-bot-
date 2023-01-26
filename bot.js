08b614ed213b6d00935e7bd31907f7597448e891
mr-Maksim0001k <maksimadamenko54@gmail.com>
2023-01-26 21:30:28 +0200

    5 commit

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
 const { Telegraf } = require("telegraf");
+const axios = require("axios");
 
 const bot = new Telegraf("5900300375:AAEfhfbZS2un47m9Us0_c89r06ghkPT1D8E");
 bot.start((ctx) => ctx.reply("Welcome"));
-bot.on("message", (ctx) => {
-  console.log("ctx.message");
+bot.on("message", async (ctx) => {
+  if (ctx.message.location) {
+    console.log(ctx.message.location);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=cf8b5f06685b88f05cee668cec61e243`;
+    const response = await axios.get(url);
+    console.log(response);
+    ctx.reply(`${response.data.name}: ${response.data.main.temp} C`);
+  }
 });
 bot.launch();
 
